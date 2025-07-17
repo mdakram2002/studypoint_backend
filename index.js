86a3d4a082a5e07c0301be81e73d51e42ee4b807
Md <mdakram12022002@gmail.com>
2025-07-18 00:47:05 +0530

    fix: CORS

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://study-point.azurewebsites.net"],
+  origin: ["http://localhost:3000", "https://studypoint-frontend.vercel.app"],
   credentials: true,
 };
 app.use(cors(corsOptions));
